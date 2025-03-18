1540a8f186d7d00ae9669bf79615a72075426a9b
Ever ATILANO <ever.atilano3001@gmail.com>
2025-03-18 21:59:10 +0100

    refactor(cli): using typer

--- cli.py
+++ cli.py
@@ -7,6 +7,7 @@ from loguru import logger
 from typing_extensions import Annotated
 from typing import Optional
 from importlib.metadata import version
+from pathlib import Path
 
 from main import main as spotify_dl_main
 
@@ -21,16 +22,17 @@ def version_callback(value: bool):
 @app.command()
 def main(
     playlist_url: str = typer.Argument(..., help="Spotify playlist URL or ID"),
-    output_dir: str = typer.Option(
-        "./downloads/",
+    output_dir: Path = typer.Option(
         "--output-dir",
         "-o",
         help="Output directory for downloading songs (default: ./downloads)",
+        default=Path.cwd() / "downloads",
     ),
     version: Annotated[
         Optional[bool], typer.Option("--version", callback=version_callback)
     ] = None,
 ):
+    """Spotify Playlist Downloader"""
     logger.info(f"Starting download from {playlist_url} to {output_dir}")
     try:
         spotify_dl_main(playlist_url, output_dir)
